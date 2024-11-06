94c802fd0f2e568d110fa241b35066cce1a288c6
mangasamudram_sruthi <sruthianand1234@gmail.com>
2024-11-06 10:41:32 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Import the Express library
+const express = require('express');
+const app = express();
+
+// Define the port to run the server
+const PORT = 3000;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
